c6b24a594f3abe073898c0d0618e3493ff7ff300
lakuzn <91047299+lakuzn@users.noreply.github.com>
2022-12-05 20:52:21 +0300

    commit with utf-8

---
+++ bucketsort.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int findmax(int arr[], int size) {
+    int cnt = 0;
+    int max = arr[0];
+    for (int i = 1; i < size; i++) //находим максимум в массиве
+    {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    while (max > 0) //находим его разряд, чтобы разложить каждый разряд в свой bucket
+    {
+        cnt++;
+        max = max / 10;
+    }
+    return cnt;
+}
+
+void bucketsort(int arr[], int* bucket[], int n)
+{
+    static int j[10], d = 1; 
+    int c, k;
+    c = findmax(arr, n); // находим максимум массива
+    for (int m = 0; m < c; m++)
+    {
+        for (int i = 0; i < 10; i++) //разбираем массив на бакеты каждого разряда. 
+            j[i] = 0;
+        for (int i = 0; i < n; i++)
+        {
+            k = (arr[i] / d) % 10;
+            bucket[k][j[k]] = arr[i]; //распределяем бакеты.
+            j[k]++;
+        }
+
+        int l = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            for (int k = 0; k < j[i]; k++) //сортируем каждый бакет отдельно
+            {
+                arr[l] = bucket[i][k];
+                l++;
+            }
+        }
+        d *= 10;
+    }
+}
+int main() //мейн, в котором мы вводим массив, применяем bucketsort и выводим отсортированный массив.
+{
+    setlocale(LC_ALL, "RUS");
+    int size;
+    int* bucket[10];
+    cout << "Введите размер массива." << endl;
+    cin >> size;
+    int* arr = new int[size + 1];
+    for (int i = 0; i < 10; i++)
+        bucket[i] = new int[size];
+    cout << "Введите элемент массива." << endl;
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+    bucketsort(arr, bucket, size);
+    cout << "Отсортированный массив:" << endl;
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
---
+++ insertionsort.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>  
+#include <math.h>
+using namespace std;
+
+void insertionsort(int* arr, int size) {
+	for (int i = 1; i < size; i++) //мы начинаем со второго элемента и сравниваем с предыдущим. Аналогичные действия производим с каждым элементов массива и постепенно отсортировываем массив.
+	{
+		int temp = arr[i]; 
+		int j = i - 1;
+		while (j >= 0 && temp <= arr[j]) //пока j>=0 и взятый элемент массива меньше предыдущего, меняем их местами.
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = temp;
+	}
+}
+
+int main() { //мейн, в котором мы вводим массив, применяем insertionsort и выводим отсортированный массив
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите размер массива." << endl;
+	int size;
+	cin >> size;
+	int* arr = new int[size];
+	arr[0] = 5;
+	cout << "Введите элемент массива." << endl;
+	for (int i = 0; i < size; i++) cin >> arr[i];
+	insertionsort(arr, size);
+	cout << "Отсортированный массив:" << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
